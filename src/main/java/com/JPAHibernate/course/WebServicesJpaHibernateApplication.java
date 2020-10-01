c75d27a414cf5ee89e1a8f099d5951f2ee76ae02
Mateus Galdino <mateusgaldino1000@hotmail.com>
2020-10-01 18:47:12 -0300

    Project created

---
+++ src/main/java/com/JPAHibernate/course/WebServicesJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.JPAHibernate.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServicesJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServicesJpaHibernateApplication.class, args);
+	}
+
+}
