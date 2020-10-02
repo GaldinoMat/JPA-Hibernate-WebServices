0922e41757b396cdf29f2b5f9954120e83917139
Mateus Galdino <mateusgaldino1000@hotmail.com>
2020-10-02 15:42:47 -0300

    Order entity and dates(ISO 8601)

--- src/main/java/com/JPAHibernate/course/repositories/UserRepository.java
+++ src/main/java/com/JPAHibernate/course/repositories/UserRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.JPAHibernate.course.entities.User;
 
-public interface UserRepository extends JpaRepository<User, Long>{
+public interface UserRepository extends JpaRepository<User, Long> {
 }
